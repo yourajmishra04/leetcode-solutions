4a24faf14a50847b861d69b7ac640b263cbd6d64
YOURAJ KUMAR MISHRA <yourajmishra04@gmail.com>
2026-07-23 10:39:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (100.00%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int bob,alice;
+    bool flg;
+      void solve(vector<int>& piles , int i , int j ){
+                
+
+ 
+      }
+    bool stoneGame(vector<int>& piles) {
+        return 1;
+        bob=0;
+        alice=0;
+        flg=0;
+        int n=piles.size();
+        solve(piles, 0, n-1);
+
+    }
+};
