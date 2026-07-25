5983e6d05b1f26f05a77bb62492d04b3742c86fa
YOURAJ KUMAR MISHRA <yourajmishra04@gmail.com>
2026-07-25 09:22:34 +0530

    Time: 66 ms (92.07%) | Memory: 113 MB (59.91%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    int n, m;
+    queue<pair<int , int>> q;
+
+
+    void solve( vector<vector<int>>& v ) {
+       while(!q.empty()){
+       auto it= q.front();
+       q.pop();
+       int i=it.first , j=it.second ;
+           if(i>0 && v[i-1][j]== INT_MAX){
+               
+                v[i-1][j]=v[i][j]+1;
+                q.push({i-1 , j});
+
+           }
+           if(i<n-1 && v[i+1][j]==INT_MAX){
+                  v[i+1][j]=v[i][j]+1;
+                q.push({i+1 , j});
+
+           }
+           if(j>0 && v[i][j-1]==INT_MAX){
+                   v[i][j-1]=v[i][j]+1;
+                q.push({i , j-1});
+
+           }
+           if(j<m-1 && v[i][j+1]==INT_MAX){
+                   v[i][j+1]=v[i][j]+1;
+                q.push({i , j+1});
+
+           }
+ 
+        
+       }
+    }
+
+    vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
+        n = mat.size();
+        m = mat[0].size();
+
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+              if(mat[i][j]==0)  {
+                mat[i][j]=1;
+              }
+              else mat[i][j]=0;
+            }
+        }
+
+
+        q=queue<pair<int,int>>();
+        vector<vector<int>> v(n, vector<int>(m, INT_MAX));
+
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+              if(mat[i][j]==0)  {
+                q.push({i,j});
+                v[i][j]=0;
+              }
+            }
+        }
+
+        solve(v);
+        return v;
+    }
+};
