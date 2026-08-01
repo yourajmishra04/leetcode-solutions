e21644d5a778ce7c18cfbde379f2e1da18ec6b39
YOURAJ KUMAR MISHRA <yourajmishra04@gmail.com>
2026-08-01 09:47:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (32.54%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+int p1,p2;
+  
+
+
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+
+      
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                dp[i][j] = max(
+                    nums[i] - dp[i+1][j],
+                    nums[j] - dp[i][j-1]
+                );
+            }
+        }
+
+        return dp[0][n-1] >= 0;
+    
+    }
+};
