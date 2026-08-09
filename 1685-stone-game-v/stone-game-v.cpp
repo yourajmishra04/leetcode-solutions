b06602b7da86d9815ca3fb66af1f49ed27822bf9
YOURAJ KUMAR MISHRA <yourajmishra04@gmail.com>
2026-08-09 09:00:29 +0530

    Time: 412 ms (60.36%) | Memory: 27.8 MB (33.00%) - LeetSync

---
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+vector<int> stone;
+vector<vector<int>>dp;
+    int solve(int l , int r){
+        if(l==r) return 0;
+         if(dp[l][r]!=-1) return dp[l][r];
+        int ans=0,sum=0;
+        for(int i=l;i<=r;i++) sum+=stone[i];
+        sum/=2;
+        int temp=0;
+
+        for(int i=l;i<=r;i++){
+            temp+=stone[i];
+            if(temp>sum) break;
+            ans=max(ans , temp+ solve(l,i));
+        }
+        temp=0;
+           for(int i=r;i>=l;i--){
+            temp+=stone[i];
+            if(temp>sum) break;
+            ans=max(ans , temp+ solve(i,r));
+        }
+            return dp[l][r]=ans;
+    }
+
+  
+    int stoneGameV(vector<int>& Stone) {
+        stone =Stone;
+        int n= stone.size();
+         
+         dp.assign(n+1,vector<int>(n+1,-1));
+
+        return solve(0,n-1);
+
+    }
+};
