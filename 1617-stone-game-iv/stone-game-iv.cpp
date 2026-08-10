765930ac20a95787ba677d3266fd39007d5c9bbb
YOURAJ KUMAR MISHRA <yourajmishra04@gmail.com>
2026-08-10 10:03:33 +0530

    Time: 254 ms (11.41%) | Memory: 38 MB (9.65%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
-    bool winnerSquareGame(int n) {
-        vector<bool>dp(n+1);
-        dp[0]=0;
-        dp[1]=1;
+    vector<vector<int>> dp;
+    bool chk(int n) {
+        int k = sqrt(n);
+        if (k * k == n)
+            return 1;
+        return 0;
+    }
 
-        for(int i=2;i<n+1;i++){
-            for(int j=1;j*j<=i;j++){
-                if(dp[i-(j*j)]== 0) {
-                    dp[i]=1;
-                    break;
-                }
-            }
+    bool solve(int n, bool flg) {
+        bool ans;
+        if (n == 0) {
+            if (flg == 0)
+                return dp[n][flg] = 1;
+            else
+                return dp[n][flg] = 0;
         }
-        return dp[n];
+        if (dp[n][flg] != -1)
+            return dp[n][flg];
+        if (flg == 1) {
+            ans = 0;
+
+            for (int i = 1; i*i <= n; i++)
+               
+                    ans = ans || solve(n - i*i, 0);
+        } else {
+            ans = 1;
+            for (int i = 1; i*i <= n; i++)
+              
+                    ans = ans && solve(n - i*i, 1);
+        }
+        return dp[n][flg] = ans;
+    }
+
+    bool winnerSquareGame(int n) {
+        dp.assign(n + 1, vector<int>(2, -1));
+        bool flg = 1;
+        return solve(n, flg);
     }
 };
