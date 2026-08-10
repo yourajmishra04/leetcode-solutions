979a3ec093aafe566b9b57bc38f765d77a0059f4
YOURAJ KUMAR MISHRA <yourajmishra04@gmail.com>
2026-08-10 09:56:09 +0530

    Time: 30 ms (83.21%) | Memory: 9 MB (82.55%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool>dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+
+        for(int i=2;i<n+1;i++){
+            for(int j=1;j*j<=i;j++){
+                if(dp[i-(j*j)]== 0) {
+                    dp[i]=1;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
