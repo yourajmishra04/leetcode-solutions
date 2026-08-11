6e5eafc776e529b9b59869d55efccbe5da459dfe
YOURAJ KUMAR MISHRA <yourajmishra04@gmail.com>
2026-08-11 22:42:54 +0530

    Time: 4 ms (63.13%) | Memory: 18.4 MB (88.76%) - LeetSync

---
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int i=0,j=s.size()-1;
+        while(i<j){
+            if(s[i]<s[j]) s[j]=s[i];
+            if(s[j]<s[i]) s[i]=s[j];
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
