4d8bf93a1d6f2affc7829de4582181b5cca40198
YOURAJ KUMAR MISHRA <yourajmishra04@gmail.com>
2026-08-17 08:57:34 +0530

    Time: 402 ms (67.30%) | Memory: 27.7 MB (51.81%) - LeetSync

--- 1685-stone-game-v/stone-game-v.cpp
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
-vector<int> stone;
-vector<vector<int>>dp;
-    int solve(int l , int r){
-        if(l==r) return 0;
-         if(dp[l][r]!=-1) return dp[l][r];
-        int ans=0,sum=0;
-        for(int i=l;i<=r;i++) sum+=stone[i];
-        sum/=2;
-        int temp=0;
+    vector<int> stone;
+    vector<vector<int>> dp;
+    int solve(int l, int r) {
+        if (l == r)
+            return 0;
+        if (dp[l][r] != -1)
+            return dp[l][r];
+        int ans = 0, sum = 0;
+        for (int i = l; i <= r; i++)
+            sum += stone[i];
+        sum /= 2;
+        int temp = 0;
 
-        for(int i=l;i<=r;i++){
-            temp+=stone[i];
-            if(temp>sum) break;
-            ans=max(ans , temp+ solve(l,i));
+        for (int i = l; i <= r; i++) {
+            temp += stone[i];
+            if (temp > sum)
+                break;
+            ans = max(ans, temp + solve(l, i));
         }
-        temp=0;
-           for(int i=r;i>=l;i--){
-            temp+=stone[i];
-            if(temp>sum) break;
-            ans=max(ans , temp+ solve(i,r));
+        temp = 0;
+        for (int i = r; i >= l; i--) {
+            temp += stone[i];
+            if (temp > sum)
+                break;
+            ans = max(ans, temp + solve(i, r));
         }
-            return dp[l][r]=ans;
+        return dp[l][r] = ans;
     }
 
-  
     int stoneGameV(vector<int>& Stone) {
-        stone =Stone;
-        int n= stone.size();
-         
-         dp.assign(n+1,vector<int>(n+1,-1));
+        stone = Stone;
+        int n = stone.size();
 
-        return solve(0,n-1);
+        dp.assign(n + 1, vector<int>(n + 1, -1));
 
+        return solve(0, n - 1);
     }
 };
