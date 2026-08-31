13602e21430428c4ad8742053bab55753ed97658
YOURAJ KUMAR MISHRA <yourajmishra04@gmail.com>
2026-08-31 07:55:35 +0530

    Time: 4 ms (57.39%) | Memory: 124.7 MB (60.57%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,42 +1,45 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        int idx = 1;
-        int idxf = -1;
-        int idxs = -1;
+        if(head->next == NULL) return {-1,-1};
         ListNode* a = head;
-        ListNode* b = head->next;
+        ListNode* b=head->next;
         ListNode* c = b->next;
-        int mind = INT_MAX;
+        int idx=1;
+        int f=-1, s=-1;
+        int idxf=-1, idxs=-1, mind=INT_MAX;
+        while(c!=NULL){
+            if(b->val > a->val && b->val > c->val || b->val < a->val && b->val < c->val){
 
-        int f = -1;
-        int s = -1;
-        if (c == NULL)
-            return {-1, -1};
-        while (c) {
-            if (b->val > a->val && b->val > c->val ||
-                b->val < a->val && b->val < c->val) {
-                if (idxf == -1)
-                    idxf = idx;
-                else
-                    idxs = idx;
-            
-            f = s;
-            s = idx;
-            if (f != -1) {
-                int d = s - f;
-                mind = min(mind, d);
-            }}
-            a = a->next;
-            b = b->next;
-            c = c->next;
-            idx++;
+                if(idxf==-1) idxf=idx;
+                else idxs=idx;
+                  
+                  f=s;
+                  s=idx;
+                  if(f!=-1){
+                  mind= min(mind, s-f );
+                  }
+                
+
+
+            }
+              a=a->next;
+                  b=b->next;
+                  c=c->next;
+                  idx++;
         }
-        if (idxs == -1)
-            return {-1, -1};
-        int maxd = idxs - idxf;
+        if(idxs==-1) return {-1,-1};
+        return {mind , idxs-idxf};
 
-        return {mind, maxd};
     }
 };
