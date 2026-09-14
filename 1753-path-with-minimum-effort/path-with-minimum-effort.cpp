5ce8e05bb719c9a3f8115a2c6d6b71383300566d
YOURAJ KUMAR MISHRA <yourajmishra04@gmail.com>
2026-09-14 12:26:17 +0530

    Time: 110 ms (20.62%) | Memory: 22.5 MB (98.86%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -4,18 +4,20 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         n = heights.size();
         m = heights[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, INT_MAX));
-        queue<tuple<int, int, int>> q;
+        
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
+                       greater<tuple<int, int, int>>>
+            q;
         q.push({0, 0, 0});
+        vector<vector<int>> vis(n, vector<int>(m, INT_MAX));
         int ans = INT_MAX;
         while (!q.empty()) {
 
-            auto [a, b, c] = q.front();
+            auto [a, b, c] = q.top();
             q.pop();
             if (a == n - 1 && b == m - 1) {
-                ans = min(ans, c);
+                return c;
             }
-
             if (a + 1 < n && vis[a + 1][b] > max(c, abs(heights[a][b] -
                                                         heights[a + 1][b]))) {
                 vis[a + 1][b] = max(c, abs(heights[a][b] - heights[a + 1][b]));
@@ -34,8 +36,8 @@ public:
                 q.push(
                     {a - 1, b, max(c, abs(heights[a][b] - heights[a - 1][b]))});
             }
-            if (b - 1 >= 0 &&
-                vis[a][b-1] > max(c, abs(heights[a][b] - heights[a][b - 1]))) {
+            if (b - 1 >= 0 && vis[a][b - 1] > max(c, abs(heights[a][b] -
+                                                         heights[a][b - 1]))) {
                 vis[a][b - 1] = max(c, abs(heights[a][b] - heights[a][b - 1]));
                 q.push(
                     {a, b - 1, max(c, abs(heights[a][b] - heights[a][b - 1]))});
